a30ed2fe286d043937c026ccdbfcd9f3285165d5
tangent500 <tangent500@yahoo.co.jp>
2020-05-11 06:28:47 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_071125) do
+ActiveRecord::Schema.define(version: 2020_05_11_011729) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
