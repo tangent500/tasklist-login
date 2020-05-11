40b8bf50dbac179865bada1773c2d787c7405f73
tangent500 <tangent500@yahoo.co.jp>
2020-05-11 09:21:39 +0000

    CRUD tasks

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_011729) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "status"
+    t.string "status"
   end
 
 end
